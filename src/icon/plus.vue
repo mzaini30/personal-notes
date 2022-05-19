03a26575d788a4342ced82e9c0444b93ae4110b5
mzaini30 <muhzaini30@gmail.com>
2022-05-20 06:33:40 +0800

    Kirim

---
+++ src/icon/plus.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg class="w-10 h-10" viewBox="0 0 32 32"><path fill="currentColor" d="M17 15V8h-2v7H8v2h7v7h2v-7h7v-2z"></path></svg>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,2 +1,9 @@
+<script setup>
+	import Plus from "../icon/plus.vue"
+</script>
+
 <template>
+	<router-link class="fixed bottom-0 right-0 m-2 block bg-green-500 rounded-[50%] text-white" to="/add">
+		<Plus/>
+	</router-link>
 </template>
